97985af178c0460f79c2472474b78d3b50f8e4f2
Alex <alex.rodrigues23997@gmail.com>
2022-09-22 14:56:44 -0300

    Taking notes on ECS steps and load-balancers

---
+++ section_9/deployment-04-multi-container-starting-setup/backend/app.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Goal = require('./models/goal');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/goals', async (req, res) => {
+  console.log('TRYING TO FETCH GOALS');
+  try {
+    const goals = await Goal.find();
+    res.status(200).json({
+      goals: goals.map((goal) => ({
+        id: goal.id,
+        text: goal.text,
+      })),
+    });
+    console.log('FETCHED GOALS');
+  } catch (err) {
+    console.error('ERROR FETCHING GOALS');
+    console.error(err.message);
+    res.status(500).json({ message: 'Failed to load goals.' });
+  }
+});
+
+app.post('/goals', async (req, res) => {
+  console.log('TRYING TO STORE GOAL');
+  const goalText = req.body.text;
+
+  if (!goalText || goalText.trim().length === 0) {
+    console.log('INVALID INPUT - NO TEXT');
+    return res.status(422).json({ message: 'Invalid goal text.' });
+  }
+
+  const goal = new Goal({
+    text: goalText,
+  });
+
+  try {
+    await goal.save();
+    res
+      .status(201)
+      .json({ message: 'Goal saved', goal: { id: goal.id, text: goalText } });
+    console.log('STORED NEW GOAL');
+  } catch (err) {
+    console.error('ERROR FETCHING GOALS');
+    console.error(err.message);
+    res.status(500).json({ message: 'Failed to save goal.' });
+  }
+});
+
+app.delete('/goals/:id', async (req, res) => {
+  console.log('TRYING TO DELETE GOAL');
+  try {
+    await Goal.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: 'Deleted goal!' });
+    console.log('DELETED GOAL');
+  } catch (err) {
+    console.error('ERROR FETCHING GOALS');
+    console.error(err.message);
+    res.status(500).json({ message: 'Failed to delete goal.' });
+  }
+});
+
+setTimeout(() => {
+ 
+  mongoose.connect(
+    `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.HOST_MONGO_DATABASE}:27017/course-goals?authSource=admin`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+  
+      if (err) {
+        console.log("Username: " + process.env.MONGO_INITDB_ROOT_USERNAME);
+        console.log("Password: " + process.env.MONGO_INITDB_ROOT_PASSWORD);
+        console.log("Host: " + process.env.HOST_MONGO_DATABASE);
+        console.error('FAILED TO CONNECT TO MONGODB');
+        console.error(err);
+      } else {
+        console.log("\nUsername: " + process.env.MONGO_INITDB_ROOT_USERNAME);
+        console.log("Password: " + process.env.MONGO_INITDB_ROOT_PASSWORD);
+        console.log("Host: " + process.env.HOST_MONGO_DATABASE);
+        console.log('CONNECTED TO MONGODB!!');
+        app.listen(80);
+      }
+    }
+  );
+
+}, 18000);
+
